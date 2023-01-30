124c5922113a8b2e657766baedaf6767b328fdfa
oyo1 <oyo1@DESKTOP-LVB3P7V>
2023-01-30 21:49:20 +0700

    Add Range class

--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RangeTask
+﻿namespace RangeTask
 {
     internal class Range
     {
