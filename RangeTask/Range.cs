5c67d42445cde0ee659fb39e43a6a12708d0115f
oyo1 <oyo1@DESKTOP-LVB3P7V>
2023-01-30 21:46:49 +0700

    Add Range class

--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -8,5 +8,24 @@ namespace RangeTask
 {
     internal class Range
     {
+        public double From { get; set; }
+
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number >= From && number <= To;
+        }
     }
 }
--- RangeTask/RangeTask.cs
+++ RangeTask/RangeTask.cs
@@ -4,9 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Использование класса Range.");
 
-            Range p = new Range();
+            Console.WriteLine("Введите начало диапазона:");
+            double from = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите конец диапазона:");
+            double to = Convert.ToDouble(Console.ReadLine());
+
+            Range range = new Range(from, to);
+
+            Console.WriteLine($"Длина диапазона от {range.From} до {range.To} => {range.GetLength()}");
+
+            Console.WriteLine("Введите число для проверки принадлежности к данному диапазону:");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            if (range.IsInside(number))
+            {
+                Console.WriteLine("Число в диапазоне!");
+            }
+            else
+            {
+                Console.WriteLine("Число не в диапазоне!");
+            }
         }
     }
 }
