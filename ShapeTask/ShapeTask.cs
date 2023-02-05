cd005d54ce25f28f2baef71106d6c8ae569233a8
oyo1 <oyo1@DESKTOP-LVB3P7V>
2023-02-05 22:48:18 +0700

    add 1

--- ShapeTask/ShapeTask.cs
+++ ShapeTask/ShapeTask.cs
@@ -4,10 +4,30 @@
     {
         static void Main(string[] args)
         {
-            IShape t = new Triangle(2, 5, 5, 5, 12, 2);
+            double X1 = 2;
+            double Y1 = 1;
+
+            double X2 = 5;
+            double Y2 = 5;
+
+            double X3 = 12;
+            double Y3 = 2;
+
+            IShape t = new Triangle(X1, Y1, X2, Y2, X3, Y3);
 
             Console.WriteLine($"Площадь = {t.GetArea()}");
+
+            double sideLengthAB = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+            double sideLengthBC = Math.Sqrt(Math.Pow(X3 - X2, 2) + Math.Pow(Y3 - Y2, 2));
+            double sideLengthAC = Math.Sqrt(Math.Pow(X3 - X1, 2) + Math.Pow(Y3 - Y1, 2));
+
+            Console.WriteLine($"AB = {sideLengthAB}");
+            Console.WriteLine($"BC = {sideLengthBC}");
+            Console.WriteLine($"AC = {sideLengthAC}");
+
             Console.WriteLine($"Периметр = {t.GetPerimeter()}");
+            Console.WriteLine($"Высота = {t.GetHeight()}");
+            Console.WriteLine($"Ширина = {t.GetWidth()}");
         }
     }
 }
