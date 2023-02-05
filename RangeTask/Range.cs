59d3ad39373ba35dc38397c8c195cfe3eef7ea9f
oyo1 <oyo1@DESKTOP-LVB3P7V>
2023-02-05 14:37:39 +0700

    Updated Range class

--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -12,6 +12,10 @@
             To = to;
         }
 
+        public Range() : this(0, 0)
+        {
+        }
+
         public double GetLength()
         {
             return To - From;
@@ -20,6 +24,85 @@
         public bool IsInside(double number)
         {
             return number >= From && number <= To;
+        }
+
+        public Range GetIntersections(Range range)
+        {
+            Range newRange = new Range();
+
+            if (range.From >= From && range.From <= To)
+            {
+                newRange.From = range.From;
+
+                if (range.To < To)
+                {
+                    newRange.To = range.To;
+
+                    return newRange;
+                }
+
+                newRange.To = To;
+
+                return newRange;
+            }
+
+            return null;
+        }
+
+        public Range[] GetMerger(Range range)
+        {
+            Range[] newRanges = new Range[2];
+
+            newRanges[0] = new Range();
+            newRanges[1] = new Range();
+
+            if (To < range.From || From > range.To)
+            {
+                newRanges[0].From = From;
+                newRanges[0].To = To;
+                
+                newRanges[1].From = range.From;
+                newRanges[1].To = range.To;
+
+                return newRanges;
+            }
+
+            newRanges[0].From = Math.Min(From, range.From);
+            newRanges[0].To = Math.Max(To, range.To);
+            
+            return newRanges;
+        }
+
+        public Range[] GetDifference(Range range)
+        {
+            Range[] newRanges = new Range[2];
+            
+            newRanges[0] = new Range();
+            newRanges[1] = new Range();
+
+            if (From == range.From && range.To >= To)
+            {
+                return newRanges;
+            }
+
+            if (GetIntersections(range) is null)
+            {
+                newRanges[0].From = From;
+                newRanges[0].To = To;
+
+                return newRanges;
+            }
+
+            newRanges[0].From = From; 
+            newRanges[0].To = range.From;
+                
+            if (range.To < To)
+            {
+                newRanges[1].From = range.To;
+                newRanges[1].To = To;
+            }
+
+            return newRanges;
         }
     }
 }
--- RangeTask/RangeTask.cs
+++ RangeTask/RangeTask.cs
@@ -1,31 +1,65 @@
-﻿namespace RangeTask
+﻿using System;
+
+namespace RangeTask
 {
     internal class RangeTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Использование класса Range.");
+            Console.WriteLine("Использование класса Range*.");
 
-            Console.WriteLine("Введите начало диапазона:");
-            double from = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите начало первого диапазона:");
+            double from1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Введите конец диапазона:");
-            double to = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите конец первого диапазона:");
+            double to1 = Convert.ToDouble(Console.ReadLine());
 
-            Range range = new Range(from, to);
+            Range range1 = new Range(from1, to1);
 
-            Console.WriteLine($"Длина диапазона от {range.From} до {range.To} => {range.GetLength()}");
+            Console.WriteLine("Введите начало второго диапазона:");
+            double from2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Введите число для проверки принадлежности к данному диапазону:");
-            double number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите конец второго диапазона:");
+            double to2 = Convert.ToDouble(Console.ReadLine());
 
-            if (range.IsInside(number))
+            Range range2 = new Range(from2, to2);
+
+            Console.WriteLine($"Диапазон 1 = ({range1.From}, {range1.To})");
+            Console.WriteLine($"Диапазон 2 = ({range2.From}, {range2.To})");
+            Console.WriteLine();
+
+            Range intersectionsRange = new Range();
+            intersectionsRange = range1.GetIntersections(range2);
+
+            if (intersectionsRange is not null)
             {
-                Console.WriteLine("Число в диапазоне!");
+                Console.WriteLine($"Пересечения: ({intersectionsRange.From}, {intersectionsRange.To})");
             }
             else
             {
-                Console.WriteLine("Число не в диапазоне!");
+                Console.WriteLine("Пересечения нет (Range is null).");
+            }
+
+            Range[] mergerRanges = new Range[2];
+            mergerRanges = range1.GetMerger(range2);
+
+            Console.WriteLine();
+            Console.WriteLine($"Объединения:");
+
+            foreach (var range in mergerRanges)
+            {
+                Console.WriteLine($"({range.From}, {range.To})");
+            }
+
+            Range[] differenceRanges = new Range[2];
+            differenceRanges = range1.GetDifference(range2);
+
+            Console.WriteLine();
+            Console.WriteLine($"Разность:");
+
+            foreach (var range in differenceRanges)
+            {
+                Console.WriteLine($"({range.From}, {range.To})");
             }
         }
     }
