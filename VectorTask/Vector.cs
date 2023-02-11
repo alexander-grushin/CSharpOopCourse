f49dcd284cb608fe712823fe10a3ebc00811db31
oyo1 <oyo1@DESKTOP-LVB3P7V>
2023-02-11 14:51:33 +0700

    Add sample

---
+++ VectorTask/Vector.cs
@@ -0,0 +1,46 @@
+﻿namespace VectorTask
+{
+    public class Vector
+    {
+        private int n;
+
+        private double[] vectorComponents;
+
+        private int N
+        {
+            get 
+            { 
+                return n;
+            }
+            set 
+            { 
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Размерность n <= 0");
+                }    
+                else
+                {
+                    n = value;
+                }
+            }
+        }
+
+        public Vector(int n)
+        {
+            N = n;
+
+            vectorComponents = new double[n];
+        }
+
+        public int GetSize()
+        {
+            return N;
+        }
+
+        public override string ToString()
+        {
+            return $"({string.Join(", ", vectorComponents)})";
+        }
+
+    }
+}
---
+++ VectorTask/VectorTask.cs
@@ -0,0 +1,14 @@
+﻿namespace VectorTask
+{
+    internal class VectorTask
+    {
+        static void Main(string[] args)
+        {
+            Vector vector1 = new(3);
+
+            Console.WriteLine(vector1);
+            Console.WriteLine(vector1.GetSize());
+
+        }
+    }
+}
